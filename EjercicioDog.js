03a936b7cbafa61a0218f845607dafe3b18cd759
Andres <andguja@gmail.com>
2022-08-02 19:59:08 -0500

    Clase html

---
+++ EjercicioDog.js
@@ -0,0 +1,9 @@
+function calculateDogAge(EdadCachorro,tasa){
+
+
+    console.log("Tu perrito tiene "+EdadCachorro*tasa+" años en años de perro!")
+}
+
+calculateDogAge(9,7)
+calculateDogAge(5,6)
+calculateDogAge(3,5)
